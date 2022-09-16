5cbe5e1a3ceea90a6746bb0a526ad0dd1624047b
unknown <acunass85@gmail.com>
2022-09-16 16:55:44 -0400

    Leave me alone

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://127.0.0.1:27017/micro-addy')
+mongoose.connect('mongodb://127.0.0.1:27017/micro-addy2')
 app.use(express.json())
 
 const urlSchema = new mongoose.Schema({
